f65d6fd1e1628a1276a3c70cc1d51328a835f5a2
Ryan Roelke <ryan_roelke@brown.edu>
2014-10-31 15:49:18 -0400

    renamed fail to panic

--- src/assertions.rs
+++ src/assertions.rs
@@ -7,7 +7,7 @@ macro_rules! assert_op(
                 // check both directions of equality....
                 if !((*given_val $op *expected_val) &&
                      (*expected_val $op *given_val)) {
-                    fail!("assertion failed: `(left == right) && (right == left)` \
+                    panic!("assertion paniced: `(left == right) && (right == left)` \
                            (left: `{}`, right: `{}`)", *given_val, *expected_val)
                 }
             }
@@ -22,13 +22,14 @@ macro_rules! assert_lt(
             (given_val, expected_val) => {
                 // check both directions of equality....
                 if !(*given_val < *expected_val) {
-                    fail!("assertion failed: `(left < right)` \
+                    panic!("assertion paniced: `(left < right)` \
                            (left: `{}`, right: `{}`)", *given_val, *expected_val)
                 }
             }
         }
     })
 )
+
 #[macro_export]
 macro_rules! assert_le(
     ($given:expr , $expected:expr) => ({
@@ -36,13 +37,14 @@ macro_rules! assert_le(
             (given_val, expected_val) => {
                 // check both directions of equality....
                 if !(*given_val <= *expected_val) {
-                    fail!("assertion failed: `(left <= right)` \
+                    panic!("assertion paniced: `(left <= right)` \
                            (left: `{}`, right: `{}`)", *given_val, *expected_val)
                 }
             }
         }
     })
 )
+
 #[macro_export]
 macro_rules! assert_ge(
     ($given:expr , $expected:expr) => ({
@@ -50,13 +52,14 @@ macro_rules! assert_ge(
             (given_val, expected_val) => {
                 // check both directions of equality....
                 if !(*given_val >= *expected_val) {
-                    fail!("assertion failed: `(left >= right)` \
+                    panic!("assertion paniced: `(left >= right)` \
                            (left: `{}`, right: `{}`)", *given_val, *expected_val)
                 }
             }
         }
     })
 )
+
 #[macro_export]
 macro_rules! assert_gt(
     ($given:expr , $expected:expr) => ({
@@ -64,7 +67,7 @@ macro_rules! assert_gt(
             (given_val, expected_val) => {
                 // check both directions of equality....
                 if !(*given_val > *expected_val) {
-                    fail!("assertion failed: `(left > right)` \
+                    panic!("assertion paniced: `(left > right)` \
                            (left: `{}`, right: `{}`)", *given_val, *expected_val)
                 }
             }
@@ -72,54 +75,3 @@ macro_rules! assert_gt(
     })
 )
 
-#[test]
-fn test_assert_lt() {
-    assert_lt!(0u, 1u);
-
-    let mut result : Result<(), Box<std::any::Any + Send>> = std::task::try(proc() {
-        assert_lt!(0u, 0u)
-    });
-    assert!(result.is_err());
-
-    result = std::task::try(proc() {
-        assert_lt!(1u, 0u)
-    });
-    assert!(result.is_err());
-}
-
-#[test]
-fn test_assert_le() {
-    assert_le!(0u, 1u);
-    assert_le!(0u, 0u);
-
-    let result = std::task::try(proc() {
-        assert_le!(1u, 0u)
-    });
-    assert!(result.is_err());
-}
-
-#[test]
-fn test_assert_ge() {
-    assert_ge!(1u, 0u);
-    assert_ge!(0u, 0u);
-
-    let result = std::task::try(proc() {
-        assert_ge!(0u, 1u)
-    });
-    assert!(result.is_err());
-}
-
-#[test]
-fn test_assert_gt() {
-    assert_gt!(1u, 0u);
-
-    let mut result : Result<(), Box<std::any::Any + Send>> = std::task::try(proc() {
-        assert_gt!(0u, 0u)
-    });
-    assert!(result.is_err());
-
-    result = std::task::try(proc() {
-        assert_gt!(0u, 1u)
-    });
-    assert!(result.is_err());
-}
